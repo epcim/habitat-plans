d16f80a1c227b8e409c995a10eea67fdae7a4e6a
Petr Michalec <epcim@apealive.net>
2019-05-03 13:29:34 +0200

    update vsb

--- vesbox-ipxe/config/content/isomount.sh
+++ vesbox-ipxe/config/content/isomount.sh
@@ -1,16 +1,20 @@
-#!/bin/bash
+#!/bin/bash -e
 
 VERSION="${1:-{{cfg.ves.image_revision}}}"
+[[ $VERSION =~ ^{{cfg ]] && VERSION=
 
 # find and mount latest .iso
 # mount -o loop .iso in data directory to expected netboot folders
-
 for j in "ves-re" "ves-ce" "ves-re-mini" "ves-ce-mini"; do
   for iso in $(ls vsb-$j*.iso| grep $VERSION | sort | tail -n 1); do
+    echo Found: $i
     [[ -e boot/$j ]] || mkdir -p boot/$j
     mount | grep "boot/$j" &&\
       umount -f ./boot/$j
-    mount -o loop $iso ./boot/$j || true
+    mount -o ro,loop $iso ./boot/$j || true
+    test -e boot/$j/initrd
+    test -e boot/$j/vmlinuz
+    test -e boot/$j/live/filesystem.squashfs
   done
 done
 
