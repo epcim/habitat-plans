71004958ce2ff8a2749e1c6ba4f42f99c1792e04
Petr Michalec <epcim@apealive.net>
2019-05-09 10:30:02 +0200

    fix template issue

--- vesbox-ipxe/config/content/isofetch.sh
+++ vesbox-ipxe/config/content/isofetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="${1:-{{cfg.ves.image_revision}}}"
-[[ $VERSION =~ ^{{cfg ]] && VERSION=
+[[ $VERSION =~ ^.*cfg ]] && VERSION=
 
 # fetch .iso to bootstrap from upstream servers
 test -e boot || mkdir boot
