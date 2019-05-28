48afe6763512076664daf5d613bb7bb5341542f2
Petr Michalec <epcim@apealive.net>
2019-05-28 12:35:06 +0200

    fix ves-ipxe/fetch.sh

--- vesbox-ipxe/config/content/fetch.sh
+++ vesbox-ipxe/config/content/fetch.sh
@@ -4,24 +4,22 @@ BASE=$(dirname $0)
 DATA=$BASE/../data
 VERSION="${1:-{{cfg.ves.image_revision}}}"
 
-cd $DATA
-
 # try to fetch bootstrap images from upstream servers
 test -e boot || mkdir boot
 test -e $BASE/id_rsa_imagesync && RSYNC_KEY="-i $BASE/id_rsa_imagesync" || RSYNC_KEY=""
-rsync -avh $RSYNC_KEY --include "*ipxe*" --exclude "*" \
-  "imagesync@images.vedge.io:data/" ./ || exit 0
-rsync -avh $RSYNC_KEY --include "*${VERSION}*.iso" --include "*${VERSION}*.img.bz2" --exclude "*"
-  "imagesync@images.vedge.io:data/boot/" ./boot/ || exit 0
+rsync -avh -e "ssh -o StrictHostKeyChecking=no $RSYNC_KEY" --include "*ipxe*" --exclude "*" \
+  "imagesync@images.vedge.io:data/" $DATA/ || exit 0
+rsync -avh -e "ssh -o StrictHostKeyChecking=no $RSYNC_KEY" --include "*${VERSION}*.iso" --include "*${VERSION}*.img.bz2" --exclude "*" \
+  "imagesync@images.vedge.io:data/boot/" $DATA/boot/ || exit 0
 
-# fetch boostrap ipxe images, failover
-#[[ -e ipxe.iso ]] || curl -qls http://boot.ipxe.org/ipxe.iso > boot/ipxe.iso
-#[[ -e ipxe.usb ]] || curl -qls http://boot.ipxe.org/ipxe.usb > boot/ipxe.usb.img
-#[[ -e ipxe.efi ]] || curl -qls http://boot.ipxe.org/ipxe.efi > boot/ipxe.efi.img
+# failover
+[[ -e $DATA/boot/ipxe.iso ]] || curl -qls http://boot.ipxe.org/ipxe.iso > $DATA/boot/ipxe.iso
+[[ -e $DATA/boot/ipxe.usb ]] || curl -qls http://boot.ipxe.org/ipxe.usb > $DATA/boot/ipxe.usb.img
+[[ -e $DATA/boot/ipxe.efi ]] || curl -qls http://boot.ipxe.org/ipxe.efi > $DATA/boot/ipxe.efi.img
 
 # housekeeping
 for i in re ce; do
   for j in iso img img.bz2; do
-    ls -ti ./boot/*ves-$i-*.$j | tail -n +4 | xargs -rt rm --
+    ls -ti $DATA/boot/*ves-$i-*.$j | tail -n +4 | xargs -rt rm --
   done
 done
